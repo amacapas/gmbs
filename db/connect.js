8b10a1ef5b51881c55d0f5365725e6493dbe82e1
Ariel M <ariel.macapas@gmail.com>
2021-07-11 23:46:24 +0800

    remove uri

--- db/connect.js
+++ db/connect.js
@@ -1,7 +1,6 @@
 const mongoose = require('mongoose');
 
 // mongoose.set('debug', true);
-// mongodb://symplemarket:C15WkTsIym@mongodb.stage.crawless.com:27017/admin?authSource=spiders&readPreference=primary&appname=MongoDB%20Compass&directConnection=true&ssl=false
 mongoose.connect('mongodb://localhost:27017/gmbs', {
     useNewUrlParser: true,
     useUnifiedTopology: true
